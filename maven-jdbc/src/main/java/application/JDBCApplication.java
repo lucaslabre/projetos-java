fa59da59cdc6ef860970cc3f707eb5efd38e682b
lucaslabre <48772886+lucaslabre@users.noreply.github.com>
2021-07-01 10:03:17 -0300

    Mínimo necessário para sincronizar JDBC com Postgres.

--- maven-jdbc/src/main/java/application/JDBCApplication.java
+++ maven-jdbc/src/main/java/application/JDBCApplication.java
@@ -1,13 +1,11 @@
 package application;
 
 import dao.CadastroDao;
+import model.Cadastro;
 
 public class JDBCApplication {
 	
 	public static void main(String[] args) {
-
 		CadastroDao dao = new CadastroDao();
-
 	}
-
 }
